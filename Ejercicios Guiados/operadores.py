cfb31f7aa376b57211eff32223a0c280e86585d9
8Mario8 <mario.faro@campus.xaloc.org>
2025-09-17 09:23:37 +0200

    Python

---
+++ Ejercicios Guiados/operadores.py
@@ -0,0 +1,15 @@
+#Practicar con los 7 operadores
+
+var1=float(input("Introduce un número: "))
+var2=float(input("Introduce otro número: "))
+#suma
+print("SUMA")
+print(var1)
+print(var2)
+total=var1+var2
+print("La suma es: ", total)
+#resta
+print(var1)
+print(var2)
+total=var1-var2
+print("La resta es: ", total)
---
+++ Ejercicios Guiados/practica_1.py
@@ -0,0 +1,21 @@
+
+# la instrucción "print" te permite visualizar por pantalla
+
+
+print("Hola, buemos días")
+print("Hola, buenas tardes")
+
+
+#variable numérica
+var1=4
+
+#variable decimal
+var2=4.5
+
+#variable texto
+var3="Adios"
+var4="Hola"
+
+
+print("El resultado es =", var1+var2)
+print("Le dije", var4, "y él me contesto", var3)
