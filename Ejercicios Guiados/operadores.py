7032a399763025e76fdf2200c68a701e067df6e1
8Mario8 <mario.faro@campus.xaloc.org>
2025-09-17 09:53:11 +0200

    Update operadores.py

--- Ejercicios Guiados/operadores.py
+++ Ejercicios Guiados/operadores.py
@@ -1,15 +1,48 @@
 #Practicar con los 7 operadores
 
-var1=float(input("Introduce un número: "))
-var2=float(input("Introduce otro número: "))
 #suma
 print("SUMA")
+var1=float(input("Introduce un número: "))
+var2=float(input("Introduce otro número: "))
 print(var1)
 print(var2)
-total=var1+var2
-print("La suma es: ", total)
+total_suma=var1+var2
+print("El resultado de la suma es: ", total_suma)
+
 #resta
+print("RESTA")
+var1=float(input("Introduce un número: "))
+var2=float(input("Introduce otro número: "))
 print(var1)
 print(var2)
-total=var1-var2
-print("La resta es: ", total)
+total_resta=var1-var2
+print("El resultado de la resta es: ", total_resta)
+
+#multiplicación
+print("MULTIPLICACIÓN")
+var1=float(input("Introduce un número: "))
+var2=float(input("Introduce otro número: "))
+print(var1)
+print(var2)
+total_multiplicación=var1*var2
+print("El resultado de la muliplicación es: ", total_multiplicación)
+
+#división
+print("DIVISIÓN")
+var1=float(input("Introduce un número: "))
+var2=float(input("Introduce otro número: "))
+print(var1)
+print(var2)
+total_división=var1/var2
+print("El resultado de la división es: ", total_división)
+
+#potencia
+print("POTENCIA")
+var1=float(input("Introduce un número: "))
+var2=float(input("Introduce otro número: "))
+print(var1)
+print(var2)
+total_potencia=var1**var2
+print("El resultado de la potencia es:", total_potencia)
+
+#División entera
