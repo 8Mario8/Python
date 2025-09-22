78a771965b5eee32f5ae5a169e5c60000a251219
8Mario8 <mario.faro@campus.xaloc.org>
2025-09-22 19:26:00 +0200

    Python

--- Ejercicios Iniciales/Ejercicio_6.py
+++ Ejercicios Iniciales/Ejercicio_6.py
@@ -1,4 +1,4 @@
-#5. Programa que pida cinco palabras y muestre una frase con las cinco. Modifica el código para que entre palabra y palabra haya una coma.
+#6. A partir del programa 5. Haz que se muestre por pantalla también la frase en el orden inverso en que se han introducido las palabras. 
 var1=input("Introduce una primera palabra: ")
 var2=input("Introduce una segunda palabra: ")
 var3=input("Introduce una tercera palabra: ")
@@ -9,4 +9,5 @@ print(var2)
 print(var3)
 print(var4)
 print(var5)
-print(var1,",",var2,",",var3,",",var4,",",var5)
+print(var1,var2,var3,var4,var5)
+print(var5,var4,var3,var2,var1)
