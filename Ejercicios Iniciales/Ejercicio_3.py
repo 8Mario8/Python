d4943b2e7d575e5ca41cdeeb26769d345b62affc
8Mario8 <mario.faro@campus.xaloc.org>
2025-09-22 17:30:54 +0200

    Update Ejercicio_3.py

---
+++ Ejercicios Iniciales/Ejercicio_3.py
@@ -0,0 +1,5 @@
+#3. Programa que pida dos números enteros y realice la suma correspondiente
+var1=float(input("Introduce un número: "))
+var2=float(input("Introduce otro número: "))
+total=var1+var2
+print("El resultado de la suma es: ", total)
