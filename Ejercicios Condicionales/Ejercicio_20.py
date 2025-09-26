e041624a35fb3ed39d057c67d34dc22916264aa2
8Mario8 <mario.faro@campus.xaloc.org>
2025-09-26 16:00:57 +0200

    Update Ejercicio_20.py

--- Ejercicios Condicionales/Ejercicio_20.py
+++ Ejercicios Condicionales/Ejercicio_20.py
@@ -1,12 +1,6 @@
 #20. A partir del ejercicio anterior, forzar que el usuario solo pueda introducir por teclados números entre 0 y 10
 var1=float(input("Introduce un número: "))
 var2=float(input("Introduce otro número: "))
-if var1>var2:
-    print("El número", var1, "es mayor que el número", var2)
-if var1<var2:
-    print("El número", var1, "es menor que el número", var2)
-if var1==var2:
-    print("El número", var1, "es igual que el número", var2)
 if var1>=10:
     print("Uno de los números esta fuera de los límites.")
 if var2>=10:
