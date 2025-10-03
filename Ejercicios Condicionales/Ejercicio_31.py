db31cd131afc643e6a87485c6d5136626f6160ca
8Mario8 <mario.faro@campus.xaloc.org>
2025-10-03 15:34:29 +0200

    Python

--- Ejercicios Condicionales/Ejercicio_31.py
+++ Ejercicios Condicionales/Ejercicio_31.py
@@ -1,9 +1,14 @@
 #31. Asigna a una variable de texto la siguiente frase: A quién madruga Dios ayuda. Comprueba si existen las siguientes palabras mostrando por pantalla la posición de su índice.
 def buscar_palabra(frase, palabra):
-frase="A quién madruga Dios ayuda."
-palabras=["A", "quién", "Dios", "ayuda", "madruga"]
-palabra=input("Introduce una palabra para buscar en la frase: ")
-if buscar_palabra(frase, palabra):
-    print(f"La palabra '{palabra}' está en la frase.")
+    palabras = frase.replace('.', '').split()
+    if palabra in palabras:
+        return palabras.index(palabra)
+    else:
+        return -1
+frase = "A quién madruga Dios ayuda."
+palabra = input("Introduce una palabra para buscar en la frase: ")
+indice = buscar_palabra(frase, palabra)
+if indice != -1:
+    print(f"La palabra '{palabra}' está en la frase en la posición {indice}.")
 else:
     print(f"La palabra '{palabra}' NO está en la frase.")
--- Ejercicios Condicionales/Ejercicio_32.py
+++ Ejercicios Condicionales/Ejercicio_32.py
@@ -1,10 +1,15 @@
 #32. Cómo solucionarías con ayuda de métodos String el problema del ejercicio anterior para no distinguir entre mayúsculas y minúsculas
-frase="A quién madruga Dios ayuda.".lower()
-palabras=["quién", "Dios", "ayuda", "madruga", "tarde"]
-palabra=input("Introduce una palabra para buscar en la frase: ").lower()
-for palabra in palabras:
-    indice=frase.find(palabra.lower())
-    if indice!=-1:
-        print(f"La palabra '{palabra}' se encuentra en el índice {indice}.")
+def buscar_palabra(frase, palabra):
+    palabras = frase.replace('.', '').lower().split()
+    palabra = palabra.lower()
+    if palabra in palabras:
+        return palabras.index(palabra)
     else:
-        print(f"La palabra '{palabra}' no se encuentra en la frase.")
+        return -1
+frase = "A quién madruga Dios ayuda."
+palabra = input("Introduce una palabra para buscar en la frase: ")
+indice = buscar_palabra(frase, palabra)
+if indice != -1:
+    print(f"La palabra '{palabra}' está en la frase en la posición {indice}.")
+else:
+    print(f"La palabra '{palabra}' NO está en la frase.")
