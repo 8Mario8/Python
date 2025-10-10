4115e1f1daa183b94f3173b3b58ca1f026fe22ad
8Mario8 <mario.faro@campus.xaloc.org>
2025-10-10 16:05:04 +0200

    Python

---
+++ SIMULACRO_EXAMEN/Ejercicio_1.py
@@ -0,0 +1,17 @@
+nombre=input("Inrtroduce tu nombre: ")
+edad=int(input("Introduce tu edad: "))
+if nombre.isupper():
+    if edad>0 and edad<100:
+        año_actual=2025
+        futuro=año_actual+(100-edad)
+        print("Hola",nombre,"cumplirás 100 años en el año",futuro)
+    else:
+        print("Edad no válida")
+else:
+    nombre=nombre.upper()
+    if edad>0 and edad<100:
+        año_actual=2025
+        futuro=año_actual+(100-edad)
+        print("Hola",nombre,"cumplirás 100 años en el año",futuro)
+    else:
+        print("Edad no válida")
