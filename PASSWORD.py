2181b57f96bc8dad7aa17d8bf779cf02eb59f575
8Mario8 <mario.faro@campus.xaloc.org>
2025-10-29 09:14:09 +0100

    Update PASSWORD.py

--- PASSWORD.py
+++ PASSWORD.py
@@ -1,8 +1,14 @@
-var="alcachofa"
-
-
-
-
-print(var[0])
-print(var[1])
-print(var[4])
+# Sistema de validació de contrasenyes
+## Introduir les instruccions
+print("INSTRUCCIONS")
+print("1. La longitud del password té que tenir entre 6 i 8 caràcters")
+print("2. Forçar els següents valors segons la posició indicada:")
+print("Posició 1: Un número major o igual que 1 i menor o igual que 5")
+print("Posició 2: Una lletra minúscula")
+print("Posició 3: Una lletra majúscula")
+print("Posició 4: Un dels següents símbols *, _, @")
+print("Posició 5: Una lletra minúscula")
+print("Posició 6: Un número major o igual que 6 i menor o igual que 9")
+print("Posició 7: Un dels següents símbols &, /, #")
+print("Posició 8: Un número menor o igual que 5")
+password=input("Introdueix una contrasenya: ")
