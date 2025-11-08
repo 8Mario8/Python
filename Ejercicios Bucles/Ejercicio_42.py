188d863969f9b2135a6db2217849eb4d5b74913b
8Mario8 <mario.faro@campus.xaloc.org>
2025-11-08 13:00:36 +0100

    Python

---
+++ Ejercicios Bucles/Ejercicio_42.py
@@ -0,0 +1,18 @@
+#42. Imprima el siguiente patrón con el ciclo for.
+#    * 
+#    ** 
+#    *** 
+#    **** 
+#    ***** 
+#    **** 
+#    *** 
+#    **
+#    *
+for i in range(1, 6):
+    for j in range(i):
+        print("*", end="")
+    print()
+for i in range(4, 0, -1):
+    for j in range(i):
+        print("*", end="")
+    print()
