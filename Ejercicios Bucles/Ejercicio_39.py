11c7114f4c5fb1db95d9a06673c81af4d90909b1
8Mario8 <mario.faro@campus.xaloc.org>
2025-11-08 12:43:09 +0100

    Create Ejercicio_39.py

---
+++ Ejercicios Bucles/Ejercicio_39.py
@@ -0,0 +1,19 @@
+#39.  Programa que pida n números y que, tras introducir el último número, debe aparecer por pantalla el número total de positivos, negativos y número de 0.
+num_numeros = float(input("Introduce el número de números que deseas introducir: "))
+if num_numeros != int(num_numeros):
+    print("Error")
+else:
+    positivos = 0
+    negativos = 0
+    ceros = 0
+    for i in range(int(num_numeros)):
+        numero = float(input("Introduce un número: "))
+        if numero > 0:
+            positivos += 1
+        elif numero < 0:
+            negativos += 1
+        else:
+            ceros += 1
+    print("Números positivos: ", positivos)
+    print("Números negativos: ", negativos)
+    print("Números cero: ", ceros)
