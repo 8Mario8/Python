6a7cb1c5e2b938a7c6485ad05a8b07c0dc2c0800
8Mario8 <mario.faro@campus.xaloc.org>
2025-11-08 12:34:33 +0100

    Update Ejercicio_37.py

---
+++ Ejercicios Bucles/Ejercicio_37.py
@@ -0,0 +1,11 @@
+#37. Programa que pregunte cuantas notas quiero introducir y para cada nota diga si estoy aprobado o suspendido.
+num_notas = float(input("Introduce el número de notas que deseas introducir: "))
+if num_notas < 0 or num_notas > 10 or num_notas != int(num_notas):
+    print("Error")
+else:
+    for i in range(int(num_notas)):
+        nota = float(input("Introduce la nota: "))
+        if nota >= 5:
+            print("Asignatura aprobada")
+        else:
+            print("Asignatura suspendida")
