aef0c48d592401f11feaf76d30b92b457ec82827
8Mario8 <mario.faro@campus.xaloc.org>
2025-11-11 10:34:28 +0100

    Python

--- Ejercicios Bucles/Ejercicio_44.py
+++ Ejercicios Bucles/Ejercicio_44.py
@@ -1,10 +1,4 @@
 #44. Realiza un programa que recorra todos los números comprendidos de 0 a 100 realizando saltos de 3 en 3. El resultado debe aparecer por pantalla en una línea con los números separados por ‘,’
-numeros = []
-for i in range(0, 101, 3):
-    numeros.append(str(i))
-print(",".join(numeros))
-
-# Otra forma de hacerlo sin usar listas
 for i in range(0, 101, 3):
     if i < 99:
         print(i, end=",")
---
+++ Ejercicios Bucles/Ejercicio_45.py
@@ -0,0 +1,5 @@
+#45. Realiza un programa que permita introducir una palabra por teclado y puedas recorrer el string distinguiendo vocales y las consonantes:
+palabra=input("Introduce una palabra: ")
+vocales="aeiou"
+for vocales in palabra:
+    print(vocales)
