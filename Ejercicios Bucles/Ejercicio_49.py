e76bc2a70ce6204d4b9629005fecbb888d4eeafb
8Mario8 <mario.faro@campus.xaloc.org>
2025-11-14 15:54:04 +0100

    Python

--- Ejercicios Bucles/Ejercicio_49.py
+++ Ejercicios Bucles/Ejercicio_49.py
@@ -1,1 +1,9 @@
 #49. A partir del programa anterior, modifica el código para que al introducir la letra por teclado te indique en qué posición de la palabra se encuentra la letra.
+secret=input("introduce la palabra 'secreta': ")
+for i in range(len(secret)):
+    letra=input("Introduce una letra: ")
+    if letra in secret:
+        for j in range(len(secret)):
+            print("La letra se encuentra en la posición", j+1)
+    else:
+        print("La letra no existe")
