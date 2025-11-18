d4f83c4dd3fb7cae2b59aa71698cecde9a2bb2a2
8Mario8 <mario.faro@campus.xaloc.org>
2025-11-18 10:44:25 +0100

    Python

---
+++ bucles_while.py
@@ -0,0 +1,19 @@
+#edad=int(input("Introduce la edad: "))
+
+#while edad>130 or edad<=0:
+    #print("Edad incorrecta")
+    #edad=int(input("Introduce la edad: "))
+#print("Edad correcta")
+
+
+import random
+num_secret=random.randint(1, 20)
+
+num=int(input("Introduce un número del 1 al 20: "))
+
+while num_secret!=num:
+    print("Vaya, no has acertado. :(")
+    num=int(input("Introduce un número del 1 al 20: "))
+print("Enorabuena, has acertado!!! :)")
+if num<1 and num>20:
+    
