04bd512fac09a075aa1210c1aed3c44d4cfe45c0
8Mario8 <mario.faro@campus.xaloc.org>
2026-01-09 16:03:01 +0100

    Python

--- 1er TRIMESTRE/Ejercicios Bucles While/Ejercicio_52.py
+++ 1er TRIMESTRE/Ejercicios Bucles While/Ejercicio_52.py
@@ -1,16 +1,14 @@
 #52. Realiza un programa que sume dos números enteros por teclado y presente el resultado por pantalla. El programa preguntará si deseas o no repetir la operación. Con While
-rep = True
-
-while rep == True:
-    num1 = int(input("Introduce el primer número: "))
-    num2 = int(input("Introduce el segundo número: "))
-    total = num1+num2
-    print("El resultado de la suma es: ", total)
-    repetir=input("¿Deseas repetir la operación? (s/n): ")
-
-    if repetir.lower() == 's':
-        rep = True
-
-    elif repetir.lower() == 'n':
-        rep = False
-        print("Finalizando programa...")
+rep=True
+while rep==True:
+    num1=int(input("Introduce el primer número: "))
+    num2=int(input("Introduce el segundo número: "))
+    total=num1+num2
+    print("El resultado de la suma:", total)
+    rep=input("Deseas repetir la operación s/n: ")
+    s=1
+    n=-1
+    if rep==s:
+        rep==True
+    else:
+        rep==False
--- 2er TRIMESTRE/Ejercicios Guiados 2/Ej_1.py
+++ 2er TRIMESTRE/Ejercicios Guiados 2/Ej_1.py
@@ -1,22 +1,39 @@
+# Inicializo listas
 milista = [1, 2, 3, 4, 5, 6]
 milistapor2 = []
+milistapro = []
+milistapor2pro = []
 
+# Inicializo variables
 maximo = max(milista)
 minimo = min(milista)
 suma = sum(milista)
 
+# Muestro por pantalla la lista, su máximo, su mínimo y su suma
 print(milista)
 print("maximo:", maximo)
 print("minimo:", minimo)
 print("Suma total", suma)
 
-#for x in range(len(milista)):
-#    calculo = milista[x] * 2
-#    milistapor2.append(calculo)
-#
-#print(milistapor2)
+# Hago que todos los valores de la lista se multipliquen por 2 y los muestro en otra lista de tres maneras distintas:
 
-for x in milista:
-    milistapor2.append(x * 2)
+## Primera (la opción más sencilla)
+
+for x in range(len(milista)):
+    calculo = milista[x] * 2
+    milistapor2.append(calculo)
 
 print(milistapor2)
+
+## Segunda (la opción un poco más dificil)
+
+for x in milista:
+    milistapro.append(x * 2)
+
+print(milistapro)
+
+## Tercera (la opción pro)
+
+milistapor2pro = [n * 2 for n in milista]
+
+print(milistapor2pro)
