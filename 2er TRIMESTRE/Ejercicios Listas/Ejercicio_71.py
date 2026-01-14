0baeb9c8ec0bcf57cc719bb8e2ac607a3157e8c9
8Mario8 <mario.faro@campus.xaloc.org>
2026-01-14 09:52:11 +0100

    Python

---
+++ 2er TRIMESTRE/Ejercicios Listas/Ejercicio_71.py
@@ -0,0 +1,5 @@
+#71. Haz un programa que permita al usuario introducir letras en una lista (cantidad indefinida), en esta lista no deben almacenarse las letras que se han introducido repetidas.
+letras = []
+repetir = "s"
+
+while repetir = "s":
