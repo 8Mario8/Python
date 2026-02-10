e56048c497456ac780764272ff1756d11e6b3012
8Mario8 <mario.faro@campus.xaloc.org>
2026-02-10 18:55:12 +0100

    Update P41221.py

--- 2er TRIMESTRE/JUTGE/P41221.py
+++ 2er TRIMESTRE/JUTGE/P41221.py
@@ -1,6 +1,10 @@
 numeros = input().split()
 if len(numeros) < 3:
-    numeros.append(input())
+    input_value = input()
+    if input_value != "":
+        numeros.append(input_value)
+    else:
+        numeros.append(input())
 numeros = [int(numero) for numero in numeros]
 suma = sum(numeros)
 print(suma)
