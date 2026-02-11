22a380ca96635cc2fe1b482af7c6a853c3c978f4
8Mario8 <mario.faro@campus.xaloc.org>
2026-02-11 21:03:37 +0100

    Update P92613.py

--- 2er TRIMESTRE/JUTGE/P92613.py
+++ 2er TRIMESTRE/JUTGE/P92613.py
@@ -3,7 +3,7 @@ if not numero_real.isdecimal():
     numero_real = float(numero_real)
     numero_sin_decimales_inferior = int(numero_real)
     numero_sin_decimales_superior = int(numero_real) + 1
-    numero_redondeado = round(numero_real + 0.01)
+    numero_redondeado = round(numero_real + 0.02)
 else:
     numero_sin_decimales_inferior = int(numero_real)
     numero_sin_decimales_superior = int(numero_real)
