308827ba6c3967ef995b7cf00c916fed82fd552c
8Mario8 <mario.faro@campus.xaloc.org>
2026-02-11 09:02:40 +0100

    Update P57548.py

--- 2er TRIMESTRE/JUTGE/P57548.py
+++ 2er TRIMESTRE/JUTGE/P57548.py
@@ -1,4 +1,7 @@
 numeros = input().split()
+if len(numeros) == 1:
+    numeros = numeros.append(input())
+numeros.split()
 numeros = [int(numero) for numero in numeros]
 suma = sum(numeros)
 print(suma)
