7d05175fc845bca648317f0b3ae68278384eb1d1
8Mario8 <mario.faro@campus.xaloc.org>
2026-02-12 14:27:25 +0100

    Update P34279.py

--- 2er TRIMESTRE/JUTGE/P34279.py
+++ 2er TRIMESTRE/JUTGE/P34279.py
@@ -1,23 +1,29 @@
 h = [0]
 m = [0]
 s = [0]
+h1 = 0
+m1 = 0
+s1 = 0
 hms = []
 
 hora_del_dia = input().split()
 
 h1 = int(hora_del_dia[0])
 
-if int(hora_del_dia[1]) == 59:
-    h1 = h1 + 1
-    m1 = 0
-else:
-    m1 = int(hora_del_dia[1])
-
 if int(hora_del_dia[2]) == 59:
     m1 = m1 + 1
     s1 = 0
 else:
     s1 = int(hora_del_dia[2]) + 1
+
+if int(hora_del_dia[1]) == 59:
+    h1 = h1 + 1
+    m1 = 0
+elif int(hora_del_dia[1]) == 60:
+    h1 = h1 + 1
+    m1 = 0
+else:
+    m1 = int(hora_del_dia[1])
 
 if len(str(h1)) == 1:
     h.append(h1)
