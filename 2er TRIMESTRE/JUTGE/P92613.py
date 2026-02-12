e47dcf5b0a62f1918a1d8362db39bca7d41307c7
8Mario8 <mario.faro@campus.xaloc.org>
2026-02-12 12:45:29 +0100

    Update P92613.py

--- 2er TRIMESTRE/JUTGE/P92613.py
+++ 2er TRIMESTRE/JUTGE/P92613.py
@@ -1,9 +1,15 @@
+import math
 numero_real = str(input())
 if not numero_real.isdecimal():
     numero_real = float(numero_real)
     numero_sin_decimales_inferior = int(numero_real)
     numero_sin_decimales_superior = int(numero_real) + 1
-    numero_redondeado = round(numero_real + 0.02)
+    entera = int(numero_real)
+    decimal = numero_real - entera
+    if decimal >= 0.5:
+        numero_redondeado = entera + 1
+    else:
+        numero_redondeado = entera
 else:
     numero_sin_decimales_inferior = int(numero_real)
     numero_sin_decimales_superior = int(numero_real)
