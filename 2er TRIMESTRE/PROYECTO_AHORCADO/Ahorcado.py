448a12e30e62549fabc54f4fcb21411034bf98bf
8Mario8 <mario.faro@campus.xaloc.org>
2026-02-21 20:51:10 +0100

    Update Ahorcado.py

--- 2er TRIMESTRE/PROYECTO_AHORCADO/Ahorcado.py
+++ 2er TRIMESTRE/PROYECTO_AHORCADO/Ahorcado.py
@@ -29,6 +29,10 @@ while empezar.lower() not in ["s", "n"]:
 
 if empezar.lower() == "s":
 
+    inicio_juego = datetime.now()
+    fecha_inicio_juego = inicio_juego.strftime("%d-%m-%Y")
+    hora_inicio_juego = inicio_juego.strftime("%H:%M:%S")
+
     while nueva_partida.lower() == "s" and len(lista_palabrasecreta_no_utilizadas) > 0:
 
         if partidas > 0:
@@ -58,111 +62,124 @@ if empezar.lower() == "s":
                 palabra_secreta_sin_acentos = ''.join(c for c in unicodedata.normalize('NFD', palabra_secreta) if unicodedata.category(c) != 'Mn')
                 lista_partida = []
                 lista_ahorcado = []
+                letra_ahorcado = ""
+        
+        if nueva_partida.lower() == "s" or partidas == 0:
 
-        inicio = datetime.now()
-        fecha_inicio = inicio.strftime("%d-%m-%Y")
-        hora_inicio = inicio.strftime("%H:%M:%S")
+            inicio = datetime.now()
+            fecha_inicio = inicio.strftime("%d-%m-%Y")
+            hora_inicio = inicio.strftime("%H:%M:%S")
 
-        partidas += 1
+            partidas += 1
 
-        while len(palabra_secreta) > len(lista_partida):
-            lista_partida.append("_")
+            while len(palabra_secreta) > len(lista_partida):
+                lista_partida.append("_")
 
-        while len(lista_ahorcado) < 8 and "_" in lista_partida:
-            print("\nPalabra secreta: ", " ".join(lista_partida))
-            letra = input("Introduce una letra: ").lower()
+            while len(lista_ahorcado) < 8 and "_" in lista_partida:
+                print("\nPalabra secreta: ", " ".join(lista_partida))
+                letra = input("Introduce una letra: ").lower()
 
-            if letra in palabra_secreta_sin_acentos.lower():
+                if letra in palabra_secreta_sin_acentos.lower():
 
-                for i in range(len(palabra_secreta_sin_acentos)):
+                    for i in range(len(palabra_secreta_sin_acentos)):
 
-                    if palabra_secreta_sin_acentos[i].lower() == letra:
-                        lista_partida[i] = palabra_secreta[i]
-                        aciertos += 1
+                        if palabra_secreta_sin_acentos[i].lower() == letra:
+                            lista_partida[i] = palabra_secreta[i]
+                            aciertos += 1
 
-                lista_aciertos.append(letra)
+                    lista_aciertos.append(letra)
+
+                else:
+                    errores += 1
+                    if errores == 1:
+                        letra_ahorcado = "A"
+                    elif errores == 2:
+                        letra_ahorcado = "H"
+                    elif errores == 3:
+                        letra_ahorcado = "O"
+                    elif errores == 4:
+                        letra_ahorcado = "R"
+                    elif errores == 5:
+                        letra_ahorcado = "C"
+                    elif errores == 6:
+                        letra_ahorcado = "A"
+                    elif errores == 7:
+                        letra_ahorcado = "D"
+                    elif errores == 8:
+                        letra_ahorcado = "O"
+
+                    print("Letra incorrecta.")
+                    lista_ahorcado.append(letra_ahorcado)
+                    print(" ".join(lista_ahorcado))
+
+                    lista_errores.append(letra)
+
+            if "_" not in lista_partida:
+                partidas_ganadas += 1
+
+                fin = datetime.now()
+                fecha_fin = fin.strftime("%d-%m-%Y")
+                hora_fin = fin.strftime("%H:%M:%S")
+
+                total_segundos = int((fin - inicio).total_seconds())
+                minutos = total_segundos // 60
+                segundos = total_segundos % 60
+                duracion = minutos, "minutos y ", segundos, "segundos"
+                duracion = " ".join(map(str, duracion))
+
+                print("\nPalabra secreta: ", " ".join(lista_partida))
+                print("\n¡Felicidades, has adivinado la palabra!")
+                print("\nNúmero de aciertos: ", aciertos)
+                print("Número de errores: ", errores)
+                print("\nDuración de la partida: ", duracion)
+
+                linea = " -  | Inicio: ", fecha_inicio, " ", hora_inicio, " | Fin: ", fecha_fin, " ", hora_fin, " | Duración: ", duracion," | Palabra secreta: ", palabra_secreta, " | Número de aciertos: ", aciertos, " | Número de errores: ", errores,"\n"
+
+                with open("partidas_ahorcado.txt", "a", encoding="utf-8") as fichero_txt:
+                    fichero_txt.write("".join(map(str, linea)))
+
+                print("\nREGISTRANDO PARTIDA...")
+                print("Partida registrada correctamente en 'partidas_ahorcado.txt'.")
 
             else:
-                errores += 1
-                if errores == 1:
-                    letra_ahorcado = "A"
-                elif errores == 2:
-                    letra_ahorcado = "H"
-                elif errores == 3:
-                    letra_ahorcado = "O"
-                elif errores == 4:
-                    letra_ahorcado = "R"
-                elif errores == 5:
-                    letra_ahorcado = "C"
-                elif errores == 6:
-                    letra_ahorcado = "A"
-                elif errores == 7:
-                    letra_ahorcado = "D"
-                elif errores == 8:
-                    letra_ahorcado = "O"
+                partidas_perdidas += 1
 
-                print("Letra incorrecta.")
-                lista_ahorcado.append(letra_ahorcado)
-                print(" ".join(lista_ahorcado))
+                fin = datetime.now()
+                fecha_fin = fin.strftime("%d-%m-%Y")
+                hora_fin = fin.strftime("%H:%M:%S")
 
-                lista_errores.append(letra)
+                total_segundos = int((fin - inicio).total_seconds())
+                minutos = total_segundos // 60
+                segundos = total_segundos % 60
+                duracion = minutos, "minutos y ", segundos, "segundos"
+                duracion = " ".join(map(str, duracion))
 
-        if "_" not in lista_partida:
-            partidas_ganadas += 1
+                print("\nHas perdido")
+                print("\nLa palabra secreta era: ", palabra_secreta)
+                print("\nNúmero de aciertos: ", aciertos)
+                print("Número de errores: ", errores)
+                print("\nDuración de la partida: ", duracion)
 
-            fin = datetime.now()
-            fecha_fin = fin.strftime("%d-%m-%Y")
-            hora_fin = fin.strftime("%H:%M:%S")
+                linea = " -  | Inicio: ", fecha_inicio, " ", hora_inicio, " | Fin: ", fecha_fin, " ", hora_fin, " | Duración: ", duracion," | Palabra secreta: ", palabra_secreta, " | Número de aciertos: ", aciertos, " | Número de errores: ", errores,"\n"
 
-            total_segundos = int((fin - inicio).total_seconds())
-            minutos = total_segundos // 60
-            segundos = total_segundos % 60
-            duracion = minutos, "minutos y ", segundos, "segundos"
-            duracion = "".join(map(str, duracion))
+                with open("partidas_ahorcado.txt", "a", encoding="utf-8") as fichero_txt:
+                    fichero_txt.write("".join(map(str, linea)))
 
-            print("\nPalabra secreta: ", " ".join(lista_partida))
-            print("\n¡Felicidades, has adivinado la palabra!")
-            print("\nNúmero de aciertos: ", aciertos)
-            print("Número de errores: ", errores)
-            print("\nDuración de la partida: ", duracion)
-
-            linea = " -  | Inicio: ", fecha_inicio, "_", hora_inicio, " | Fin: ", fecha_fin, "_", hora_fin, " | Duración: ", duracion," | Palabra secreta: ", palabra_secreta, " | Número de aciertos: ", aciertos, " | Número de errores: ", errores,"\n"
-
-            with open("partidas_ahorcado.txt", "a", encoding="utf-8") as fichero_txt:
-                fichero_txt.write("".join(map(str, linea)))
-
-            print("\nREGISTRANDO PARTIDA...")
-            print("Partida registrada correctamente en 'partidas_ahorcado.txt'.")
-
-        else:
-            partidas_perdidas += 1
-
-            fin = datetime.now()
-            fecha_fin = fin.strftime("%d-%m-%Y")
-            hora_fin = fin.strftime("%H:%M:%S")
-
-            total_segundos = int((fin - inicio).total_seconds())
-            minutos = total_segundos // 60
-            segundos = total_segundos % 60
-            duracion = minutos, "minutos y ", segundos, "segundos"
-            duracion = "".join(map(str, duracion))
-
-            print("\nHas perdido")
-            print("\nLa palabra secreta era: ", " ".join(lista_partida))
-            print("\nNúmero de aciertos: ", aciertos)
-            print("Número de errores: ", errores)
-            print("\nDuración de la partida: ", duracion)
-
-            linea = " -  | Inicio: ", fecha_inicio, "_", hora_inicio, " | Fin: ", fecha_fin, "_", hora_fin, " | Duración: ", duracion," | Palabra secreta: ", palabra_secreta, " | Número de aciertos: ", aciertos, " | Número de errores: ", errores,"\n"
-
-            with open("partidas_ahorcado.txt", "a", encoding="utf-8") as fichero_txt:
-                fichero_txt.write("".join(map(str, linea)))
-
-            print("\nREGISTRANDO PARTIDA...")
-            print("Partida registrada correctamente en 'partidas_ahorcado.txt'.")
+                print("\nREGISTRANDO PARTIDA...")
+                print("Partida registrada correctamente en 'partidas_ahorcado.txt'.")
 
     if len(lista_palabrasecreta_no_utilizadas) == 0:
         print("\nNo quedan más palabras disponibles para jugar. Gracias por jugar al Ahorcado.")
+    
+    fin_juego = datetime.now()
+    fecha_fin_juego = fin_juego.strftime("%d-%m-%Y")
+    hora_fin_juego = fin_juego.strftime("%H:%M:%S")
+
+    total_segundos = int((fin_juego - inicio_juego).total_seconds())
+    minutos = total_segundos // 60
+    segundos = total_segundos % 60
+    duracion = minutos, "minutos y ", segundos, "segundos"
+    duracion = " ".join(map(str, duracion))
     
     print("\nREUMEN DEL JUEGO")
     print("\n Errores y Aciertos")
@@ -172,6 +189,10 @@ if empezar.lower() == "s":
     print("    Partidas jugadas: ", partidas)
     print("    Partidas ganadas: ", partidas_ganadas)
     print("    Partidas perdidas: ", partidas_perdidas)
+    print("\n Tiempo")
+    print("    Inicio del juego: ", fecha_inicio_juego, " ", hora_inicio_juego)
+    print("    Fin del juego: ", fecha_fin_juego, " ", hora_fin_juego)
+    print("    Duración total del juego: ", duracion)
 
     print("\nFIN DEL JUEGO")
 
